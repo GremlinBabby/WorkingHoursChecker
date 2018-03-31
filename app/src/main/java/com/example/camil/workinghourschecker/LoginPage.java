dcad90a4be8471cb0b2f1edd2932b3075621d616
GremlinBabby <camillamhansen@outlook.com>
2018-03-31 19:11:27 +0200

    Made register page, fixed strings and inputType, made random database references for later

--- app/src/main/java/com/example/camil/workinghourschecker/LoginPage.java
+++ app/src/main/java/com/example/camil/workinghourschecker/LoginPage.java
@@ -3,20 +3,27 @@ package com.example.camil.workinghourschecker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
 
 
-
-public class LoginPage extends Activity {
+public class LoginPage extends AppCompatActivity {
     Button login;
     AutoCompleteTextView email;
     EditText password;
 
+    //References to database
+    DatabaseReference databaseTimeStamp;
+    DatabaseReference databaseCheckIn;
+    DatabaseReference databaseCheckOut;
+    DatabaseReference databaseWorker;
+    DatabaseReference databaseAdmin;
 
 
     @Override
@@ -29,8 +36,13 @@ public class LoginPage extends Activity {
         password = findViewById(R.id.passwordInput);
 
 
+        //Initiating database
+
+
+
         //Make to check whether the email is in the database
         //THEN check if the password matches the associated password in the database. Or?
+        //If no match, go to register page
         login.setOnClickListener(new View.OnClickListener()  {
             @Override
             public void onClick(View v) {
@@ -39,5 +51,8 @@ public class LoginPage extends Activity {
             }
 
         } );
+
+
+
     }
 }
