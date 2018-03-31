f7f38dfed3fbfe416b3b711df1adc2a6121f7961
GremlinBabby <camillamhansen@outlook.com>
2018-03-31 17:58:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/camil/workinghourschecker/LoginPage.java
@@ -0,0 +1,43 @@
+package com.example.camil.workinghourschecker;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+
+public class LoginPage extends Activity {
+    Button login;
+    AutoCompleteTextView email;
+    EditText password;
+
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        login = findViewById(R.id.loginButton);
+        email = findViewById(R.id.emailInput);
+        password = findViewById(R.id.passwordInput);
+
+
+        //Make to check whether the email is in the database
+        //THEN check if the password matches the associated password in the database. Or?
+        login.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+
+        } );
+    }
+}
